0d5450f801647a7459d27262157174fa90f300e1
Mwansa <Mwansad.mwango@gmail.com>
2021-04-13 00:04:28 +0800

    added freq of washes to gauge reading, updated title

--- static/js/app.js
+++ static/js/app.js
@@ -79,42 +79,6 @@ function init() {
     
     Plotly.newPlot('bubble', data, layout);
 
-    // TODO: Plot gauge
-    var gaugeData = [
-      {
-        type: "indicator",
-        mode: "gauge+number+delta",
-        value: 420,
-        title: { text: "Speed", font: { size: 24 } },
-        delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-        gauge: {
-          axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-          bar: { color: "darkblue" },
-          bgcolor: "white",
-          borderwidth: 2,
-          bordercolor: "gray",
-          steps: [
-            { range: [0, 250], color: "cyan" },
-            { range: [250, 400], color: "royalblue" }
-          ],
-          threshold: {
-            line: { color: "red", width: 4 },
-            thickness: 0.75,
-            value: 490
-          }
-        }
-      }
-    ];
-    
-    var layout = {
-      width: 500,
-      height: 400,
-      margin: { t: 25, r: 25, l: 25, b: 25 },
-      paper_bgcolor: "lavender",
-      font: { color: "darkblue", family: "Arial" }
-    };
-    
-    Plotly.newPlot('gauge', gaugeData, layout);
     
   }
 
@@ -128,6 +92,43 @@ function init() {
       <p><strong>Belly Button Type</strong>: ${selectedMetadata.bbtype}</p>
       <p><strong>Wash Frequency</strong>: ${selectedMetadata.wfreq}</p> 
     `);
+    // TODO: Plot gauge
+    var gaugeData = [
+      {
+        type: "indicator",
+        mode: "gauge+number+delta",
+        value: selectedMetadata.wfreq,
+        title: { text: "Frequency of Washes", font: { size: 24 } },
+        delta: { reference: 0, increasing: { color: "RebeccaPurple" } },
+        gauge: {
+          axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" },
+          bar: { color: "darkred" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "gray",
+          steps: [
+            { range: [0, 250], color: "cyan" },
+            { range: [250, 400], color: "royalblue" }
+          ],
+          threshold: {
+            line: { color: "red", width: 4 },
+            thickness: 0.75,
+            value: 7
+          }
+        }
+      }
+    ];
+    
+    var layout = {
+      width: 500,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      paper_bgcolor: "white",
+      font: { color: "darkblue", family: "Arial" }
+    };
+    
+    Plotly.newPlot('gauge', gaugeData, layout);
+    
   }
   // Function find the top 10 OTU in individual sample
   function filterTop10ById(testSubjectId) {
